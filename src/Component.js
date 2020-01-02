695fd7a049769bde6196e3faa4b9fdff5c5243d8
parkwoosung <abec0125@ncomz.com>
2020-01-02 17:55:12 +0900

    add Component

--- src/Component.js
+++ src/Component.js
@@ -1,4 +1,5 @@
 import React,{Fragment} from 'react';
+import PropTypes from 'prop-types';
 
 const MyComponent = ({name, children}) => {
     // const {name, children} = props; destructuring assignment
@@ -10,12 +11,17 @@ const MyComponent = ({name, children}) => {
             <div>This is Child props : {children}</div>
         </fragment>
     )
-}
+};
 
 // props 미전달시 default props 값 지정
 MyComponent.defaultProps = {
     name : '-'
-}
+};
+
+// propTypes 를 통한 props 검증 -> 필수 props 지정 또는 자료형 지정
+MyComponent.propTypes= {
+    name : PropTypes.string
+};
 
 export default MyComponent;
 //다른 파일에서 이 파일을 import 할 때, 위의 MyComponent 클래스를 불러오도록 설정
